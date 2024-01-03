1230722b0b798f4ea42c61a46a3c900e27b94ce1
Andrew-R-Y <rakitinandrei@gmail.com>
2024-01-03 13:25:53 +0300

    feat: add canQueenCaptureKing task solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -70,8 +70,21 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+  if (queen.x - queen.y === king.x - king.y) {
+    return true;
+  }
+  if (Math.abs(queen.x - king.x) === 1 && Math.abs(queen.y - king.y) === 1) {
+    return true;
+  }
+  if (queen.x + queen.y === king.x + king.y) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
