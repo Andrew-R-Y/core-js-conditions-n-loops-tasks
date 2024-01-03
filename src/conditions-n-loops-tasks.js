055c5387d6a6b4d8a9800cff530ce223d6950462
Andrew-R-Y <rakitinandrei@gmail.com>
2024-01-03 13:32:26 +0300

    feat: add isIsoscelesTriangle task solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -105,8 +105,20 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (!a || !b || !c) {
+    return false;
+  }
+
+  if (a + b < c || a + c < b || c + b < a) {
+    return false;
+  }
+
+  if (a === b || a === c || b === c) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
